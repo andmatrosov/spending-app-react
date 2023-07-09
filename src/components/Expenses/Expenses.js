4481aac53fdb5d4e4583788ce1d8f3e99694c8a7
Andrew <a.matrosov@colynn.pro>
2023-07-09 15:34:06 +0300

    add Form and try use some useState methods

---
+++ src/components/Expenses/Expenses.js
@@ -0,0 +1,15 @@
+import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
+import './Expenses.css'
+
+const Expenses = ({ items }) => {
+	const expensesItems = items.map((item, indx) => { return <ExpenseItem title={item.title} id={item.id} amount={item.amount} date={item.date} key={indx.toString()} /> })
+	return (
+		<Card className='expenses'>
+			{expensesItems}
+		</Card>
+
+	)
+}
+
+export default Expenses;
